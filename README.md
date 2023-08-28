227e146204a3cd7706ec3be1969095821653b665
jr <141987564+jrguedes7@users.noreply.github.com>
2023-08-28 14:30:45 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # guedes
 # variavel-
+# variavel-
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "hello jr";
+// esta variavel contém o nome da pessoa 
+$nome = "jr <br>";
+echo $nome;
+
+// esta variavel contem o sexo da pessoa 
+$sexo = "Masculino <br>";
+echo $sexo;
+
+// esta variavel contém a idade da pessoa
+$idade = 24;
+echo $idade;
+
 ?>
